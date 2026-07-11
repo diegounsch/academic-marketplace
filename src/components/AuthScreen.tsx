97515287309e5ca9ea3ef4403f6f3cf33728f7ca
diegounsch <109194659+diegounsch@users.noreply.github.com>
2026-07-11 16:19:44 -0500

    Update AuthScreen.tsx

--- src/components/AuthScreen.tsx
+++ src/components/AuthScreen.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import { signInWithPopup } from "firebase/auth";
-import { auth, googleProvider } from "./firebase";
+import { auth, googleProvider } from "../firebase";
 import React, { useState } from "react";
 import { 
   BookOpen, 
